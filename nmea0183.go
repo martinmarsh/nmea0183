9e1ed16846608e46b67965c367edf2b02df0364f
Martin <martin@marshtrio.co.uk>
2022-01-11 02:44:28 +0000

    init

---
+++ nmea0183.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+)
+
+func Config(){
+	fmt.Println("hello")
+
+}
---
+++ version/version.go
@@ -0,0 +1,6 @@
+package version
+
+var (    
+    Version string = "v0.0.1-Alpha.1"
+    Build   string = "Development"
+)
